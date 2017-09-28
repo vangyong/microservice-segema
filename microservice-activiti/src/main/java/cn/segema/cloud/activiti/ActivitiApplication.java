b677aab148723fd202a01144f74c7ac0ca27959b
wangyong <vangyong@126.com>
2017-09-28 10:46:50 +0800

    调整activiti结构

--- microservice-activiti/src/main/java/cn/segema/cloud/activiti/ActivitiApplication.java
+++ microservice-activiti/src/main/java/cn/segema/cloud/activiti/ActivitiApplication.java
@@ -4,16 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-import cn.segema.cloud.activiti.modeler.JsonpCallbackFilter;
-
-
 @SpringBootApplication
 @EnableDiscoveryClient
-@ComponentScan({"org.activiti.rest.diagram", "cn.segema.cloud.activiti"})
+//@ComponentScan({"org.activiti.rest.diagram", "cn.segema.cloud.activiti"})
 @EnableAutoConfiguration(exclude = {
 		org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration.class,
 		org.activiti.spring.boot.SecurityAutoConfiguration.class,
@@ -24,10 +20,6 @@ public class ActivitiApplication extends WebMvcConfigurerAdapter {
     SpringApplication.run(ActivitiApplication.class, args);
   }
   
-  @Bean
-	public JsonpCallbackFilter filter(){
-		return new JsonpCallbackFilter();
-	}
   
 }
 
