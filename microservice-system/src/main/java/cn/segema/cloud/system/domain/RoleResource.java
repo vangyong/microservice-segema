c21c60eb8e893806205de2ba7cc2ec656799b563
vangyong <vangyong@126.com>
2017-10-08 22:31:10 +0800

    增加角色资源

---
+++ microservice-system/src/main/java/cn/segema/cloud/system/domain/RoleResource.java
@@ -0,0 +1,56 @@
+package cn.segema.cloud.system.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+/**
+ * 角色资源关系
+ * @author wangyong
+ *
+ */
+@Table(name = "SYS_ROLE_RESOURCE")
+@Entity
+public class RoleResource {
+	@Id
+	@Column(name = "ROLERESOURCEID")
+	private String roleResourceId;
+
+	@OneToOne
+	@JoinColumn(name = "ROLEID")
+	private Role role;
+
+	@OneToOne
+	@JoinColumn(name = "RESOURCEID")
+	private Resource resource;
+
+	public String getRoleResourceId() {
+		return roleResourceId;
+	}
+
+	public void setRoleResourceId(String roleResourceId) {
+		this.roleResourceId = roleResourceId;
+	}
+
+	public Role getRole() {
+		return role;
+	}
+
+	public void setRole(Role role) {
+		this.role = role;
+	}
+
+	public Resource getResource() {
+		return resource;
+	}
+
+	public void setResource(Resource resource) {
+		this.resource = resource;
+	}
+
+	
+
+}
