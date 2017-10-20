94d28e38c39a3fdae95e317c45e89734ce78e2f3
wangyong <vangyong@126.com>
2017-10-20 17:21:45 +0800

    增加合同管理内容

--- microservice-contract/src/main/java/cn/segema/cloud/contract/domain/Contract.java
+++ microservice-contract/src/main/java/cn/segema/cloud/contract/domain/Contract.java
@@ -1,6 +1,7 @@
 package cn.segema.cloud.contract.domain;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -13,10 +14,60 @@ public class Contract {
 	@Id
 	@Column(name="CONTRACTID")
 	private String contractId;
+	
 	@Column(name="CONTRACTNAME")
 	private String contractName;
+	
+	@Column(name="CONTRACTCODE")
+	private String contractCode;
+	
+	@Column(name="CONTRACTTYPE")
+	private String contractType;
+	
+	@Column(name = "STARTTIME")
+	private LocalDateTime startTime;
+	
+	@Column(name = "ENDTIME")
+	private LocalDateTime endTime;
+	
+	@Column(name = "SIGINTIME")
+	private LocalDateTime signTime;
+	
 	@Column(name="TOTALMONEY")
 	private BigDecimal totalMoney;
+	
+	@Column(name="TOTALMONEYUP")
+	private String totalMoneyUp;
+	
+	@Column(name="DISCOUNT")
+	private BigDecimal discount;
+	
+	@Column(name="PAYMENTTYPE")
+	private Integer paymentType;
+	
+	@Column(name="MARGIN")
+	private BigDecimal margin;
+	
+	@Column(name = "LASTPAYTIME")
+	private LocalDateTime lastPayTime;
+	
+	@Column(name="ADVERTISINGTYPE")
+	private Integer advertisingType;
+	
+	@Column(name="URGENTLEVEL")
+	private Integer urgentLevel;
+	
+	@Column(name="IMPORTANTLEVEL")
+	private Integer importantLevel;
+	
+	@Column(name="FUNDSSOURCE")
+	private Integer fundsSource;
+	
+	@Column(name="STATE")
+	private Integer state;
+	
+	@Column(name="ONMEDIA")
+	private Integer onMedia;
 
 	public String getContractId() {
 		return contractId;
@@ -34,6 +85,46 @@ public class Contract {
 		this.contractName = contractName;
 	}
 
+	public String getContractCode() {
+		return contractCode;
+	}
+
+	public void setContractCode(String contractCode) {
+		this.contractCode = contractCode;
+	}
+
+	public String getContractType() {
+		return contractType;
+	}
+
+	public void setContractType(String contractType) {
+		this.contractType = contractType;
+	}
+
+	public LocalDateTime getStartTime() {
+		return startTime;
+	}
+
+	public void setStartTime(LocalDateTime startTime) {
+		this.startTime = startTime;
+	}
+
+	public LocalDateTime getEndTime() {
+		return endTime;
+	}
+
+	public void setEndTime(LocalDateTime endTime) {
+		this.endTime = endTime;
+	}
+
+	public LocalDateTime getSignTime() {
+		return signTime;
+	}
+
+	public void setSignTime(LocalDateTime signTime) {
+		this.signTime = signTime;
+	}
+
 	public BigDecimal getTotalMoney() {
 		return totalMoney;
 	}
@@ -42,4 +133,92 @@ public class Contract {
 		this.totalMoney = totalMoney;
 	}
 
+	public String getTotalMoneyUp() {
+		return totalMoneyUp;
+	}
+
+	public void setTotalMoneyUp(String totalMoneyUp) {
+		this.totalMoneyUp = totalMoneyUp;
+	}
+
+	public BigDecimal getDiscount() {
+		return discount;
+	}
+
+	public void setDiscount(BigDecimal discount) {
+		this.discount = discount;
+	}
+
+	public Integer getPaymentType() {
+		return paymentType;
+	}
+
+	public void setPaymentType(Integer paymentType) {
+		this.paymentType = paymentType;
+	}
+
+	public BigDecimal getMargin() {
+		return margin;
+	}
+
+	public void setMargin(BigDecimal margin) {
+		this.margin = margin;
+	}
+
+	public LocalDateTime getLastPayTime() {
+		return lastPayTime;
+	}
+
+	public void setLastPayTime(LocalDateTime lastPayTime) {
+		this.lastPayTime = lastPayTime;
+	}
+
+	public Integer getAdvertisingType() {
+		return advertisingType;
+	}
+
+	public void setAdvertisingType(Integer advertisingType) {
+		this.advertisingType = advertisingType;
+	}
+
+	public Integer getUrgentLevel() {
+		return urgentLevel;
+	}
+
+	public void setUrgentLevel(Integer urgentLevel) {
+		this.urgentLevel = urgentLevel;
+	}
+
+	public Integer getImportantLevel() {
+		return importantLevel;
+	}
+
+	public void setImportantLevel(Integer importantLevel) {
+		this.importantLevel = importantLevel;
+	}
+
+	public Integer getFundsSource() {
+		return fundsSource;
+	}
+
+	public void setFundsSource(Integer fundsSource) {
+		this.fundsSource = fundsSource;
+	}
+
+	public Integer getState() {
+		return state;
+	}
+
+	public void setState(Integer state) {
+		this.state = state;
+	}
+
+	public Integer getOnMedia() {
+		return onMedia;
+	}
+
+	public void setOnMedia(Integer onMedia) {
+		this.onMedia = onMedia;
+	}
+	
 }
